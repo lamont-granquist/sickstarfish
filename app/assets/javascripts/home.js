a55904eaa124547dd6d9d9d10502f00d9a3adcbf
Lamont Granquist <lamont@scriptkiddie.org>
2013-12-02 17:59:46 -0800

    back to zoomed in on puget sound

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -40,7 +40,7 @@ function getStateFromURL() {
   // z = zoom
   state["z"] = $.QueryString["z"]
   if ( state["z"] == undefined ) {
-    state["z"] = 2
+    state["z"] = 9
   }
 /*  // t = tags ( can be undefined )
   state["t"] = $.QueryString["t"]
