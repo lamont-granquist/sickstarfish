1b60f4b3c308f14fb3f88fbe1a0b914b04159348
Lamont Granquist <lamont@scriptkiddie.org>
2014-03-19 22:52:31 -0700

    move secret_key_base into an env var

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Sickstarfish::Application.config.secret_key_base = '413e9c255fd43584fa7139112d88b6cd69ecc3f2c6dafecde8e812739f9e090ff5052f73e57a09e4be9ef8fd982c5f8b43efabe11739473e91d9309f76fed48c'
+Sickstarfish::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
